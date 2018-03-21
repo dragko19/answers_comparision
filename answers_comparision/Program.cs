a86ca1f55504d797a9cf10b1ce5eefaf1032e7be
dragko19 <janickihubert@gmail.com>
2018-03-21 21:55:40 +0100

    first vesrion
    basic

---
+++ answers_comparision/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace answers_comparision
+{
+    class Program
+    {
+        public static int Main()
+        {
+            Comparision test = new Comparision();
+            /*test.Set_student_answers('A', 'B', 'C', 'D');
+            test.Set_teacher_answers('A', 'B', 'E', 'D');
+
+            foreach (char elem in test._student_answers)
+                Console.WriteLine(elem);
+
+            foreach (char elem in test._teacher_answers)
+                Console.WriteLine(elem);*/
+
+            test.Compare();
+
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
